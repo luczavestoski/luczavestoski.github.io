6a8ea015b9cef5971e50fdba768f4def9e40314b
Matthew H <m.holland2021@gmail.com>
2020-05-19 03:04:03 -0700

    cleaned up index.html
    used .after() to get rid of repeating elements

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var i; //list index
+var imgNum = 21; //number of photos 
 
-//This partshould be server side PHP, but Netlify is static only
+// ^This partshould be automatic server side PHP, will do later 
 //https://stackoverflow.com/questions/11489738/how-do-i-count-files-in-a-directory-using-jquery
 
 
-for(i = 0; i < 20; i++) {
-    document.getElementsByTagName("Li")[i].innerHTML = "<img src=\"images/" +(i+1)+ ".jpg\">";
-  }
+while(imgNum > 0){
+jQuery("#repeat").after(jQuery("#repeat").first().clone());
+imgNum -= 1;
+}
 
+for(i = 0; i < (20); i++) {
+ document.getElementsByTagName("Li")[i].innerHTML = "<img src=\"images/" +(i+1)+ ".jpg\">";
+}
