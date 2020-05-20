28126d55b589ed169c32d6b2cae60224a4c19c69
Matthew H <m.holland2021@gmail.com>
2020-05-19 22:55:11 -0700

    Prep for Lazy Loading Commit

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-var imgNum = 21; //number of photos 
-var imgNum2 = imgNum; //There's probably a better way to implement this 
+var imgNum = 18; //number of photos 
+ 
+
+
+
 
 // ^This partshould be automatic server side PHP, will do later 
 //https://stackoverflow.com/questions/11489738/how-do-i-count-files-in-a-directory-using-jquery
 
 
+
 //clones #repeat <li> tags 
+var OriginalImgNum = imgNum; 
 while((imgNum-1) > 0){
 jQuery("#repeat").after(jQuery("#repeat").first().clone());
 imgNum -= 1;
 }
 
 //inserts images to <li> tags
-for(i = 0; i < (imgNum2); i++) {
- document.getElementsByTagName("Li")[i].innerHTML = "<img src=\"images/" +(i+1)+ ".jpg\">";
+for(i = 0; i < (OriginalImgNum); i++) {
+ document.getElementsByTagName("Li")[i].innerHTML = "<img src=\"images/" +(i+1)+ ".jpg\" class=\"lazy\">";
 }
+
